a00cf2681d86a66d87cac014c925bd305b3a6e88
David KRAME <kramedavid@gmail.com>
2023-04-28 13:26:30 +0200

    Fix bugs in train and exp_Model (1)

--- exp/exp_Model.py
+++ exp/exp_Model.py
@@ -20,38 +20,40 @@ import time
 import warnings
 warnings.filterwarnings('ignore')
 
+
 def RandomVariable(sample, logp, entr, **params):
     return G.Struct(sample=sample, logp=logp, entr=entr, **params)
+
 
 class Exp_Model(Exp_Basic):
     def __init__(self, args):
         super(Exp_Model, self).__init__(args)
-    
+
     def _build_model(self):
         model_dict = {
-            'Model':Model
+            'Model': Model
         }
-        if self.args.model=='Model' :
+        if self.args.model == 'Model':
             e_layers = self.args.e_layers
             model = model_dict[self.args.model](
                 self.args.enc_in,
-                self.args.dec_in, 
-                self.args.c_out, 
-                self.args.seq_len, 
+                self.args.dec_in,
+                self.args.c_out,
+                self.args.seq_len,
                 self.args.label_len,
                 self.args.pred_len,
-                self.args.step_len, 
-                self.args.d_model, 
-                self.args.n_heads, 
-                e_layers, # self.args.e_layers,
-                self.args.d_layers, 
+                self.args.step_len,
+                self.args.d_model,
+                self.args.n_heads,
+                e_layers,  # self.args.e_layers,
+                self.args.d_layers,
                 self.args.normal_layers,
                 self.args.enc_lstm,
                 self.args.dec_lstm,
                 self.args.weight,
                 self.args.window,
                 self.args.d_ff,
-                self.args.dropout, 
+                self.args.dropout,
                 self.args.attn,
                 self.args.embed,
                 self.args.freq,
@@ -60,7 +62,7 @@ class Exp_Model(Exp_Basic):
                 self.args.mix,
                 self.device
             ).float()
-        
+
         if self.args.use_multi_gpu and self.args.use_gpu:
             model = nn.DataParallel(model, device_ids=self.args.device_ids)
         return model
@@ -68,29 +70,38 @@ class Exp_Model(Exp_Basic):
     def _get_data(self, flag):
         args = self.args
         data_dict = {
-            'ETTh1':Dataset_elec,
-            'ETTh2':Dataset_elec,
-            'ETTm1':Dataset_elec,
-            'ETTm2':Dataset_elec,
-            'WTH':Dataset_elec,
-            'TRAF':Dataset_elec,
-            'EXCH':Dataset_elec,
-            'ECL':Dataset_elec,
-            'elec':Dataset_elec,
-            'air':Dataset_airdelay,
+            'ETTh1': Dataset_elec,
+            'ETTh2': Dataset_elec,
+            'ETTm1': Dataset_elec,
+            'ETTm2': Dataset_elec,
+            'WTH': Dataset_elec,
+            'TRAF': Dataset_elec,
+            'EXCH': Dataset_elec,
+            'ECL': Dataset_elec,
+            'elec': Dataset_elec,
+            'air': Dataset_airdelay,
         }
         Data = data_dict[self.args.data]
-        timeenc = 0 if args.embed!='timeF' else 1
+        timeenc = 0 if args.embed != 'timeF' else 1
 
         if flag == 'test':
-            shuffle_flag = False; drop_last = True; batch_size = args.batch_size; freq=args.freq
-        elif flag=='pred':
-            shuffle_flag = False; drop_last = False; batch_size = 1; freq=args.detail_freq
+            shuffle_flag = False
+            drop_last = True
+            batch_size = args.batch_size
+            freq = args.freq
+        elif flag == 'pred':
+            shuffle_flag = False
+            drop_last = False
+            batch_size = 1
+            freq = args.detail_freq
             #Data = Dataset_Pred
         else:
-            shuffle_flag = True; drop_last = True; batch_size = args.batch_size; freq=args.freq
+            shuffle_flag = True
+            drop_last = True
+            batch_size = args.batch_size
+            freq = args.freq
         data_set = Data(
-            data_set = args.data,
+            data_set=args.data,
             root_path=args.root_path,
             data_path=args.data_path,
             flag=flag,
@@ -103,11 +114,11 @@ class Exp_Model(Exp_Basic):
             cols=args.cols
         )
         print(flag, len(data_set))
-        
+
         for i in range(5):
             print(data_set[0][i].shape, end=' ')
             print(' ')
-        
+
         data_loader = DataLoader(
             data_set,
             batch_size=batch_size,
@@ -118,45 +129,47 @@ class Exp_Model(Exp_Basic):
         return data_set, data_loader
 
     def _select_optimizer(self):
-        model_optim = optim.Adam(self.model.parameters(), lr=self.args.learning_rate)
+        model_optim = optim.Adam(
+            self.model.parameters(), lr=self.args.learning_rate)
         return model_optim
-    
+
     def _select_criterion(self):
-        criterion =  nn.MSELoss()
+        criterion = nn.MSELoss()
         return criterion
-    
-    def loss_fn(self, mu, sigma ,labels):
+
+    def loss_fn(self, mu, sigma, labels):
         #zero_index = (labels != 0)
         distribution = torch.distributions.normal.Normal(mu, sigma)
         likelihood = distribution.log_prob(labels)
         return -torch.mean(likelihood)
-        
+
     def vali(self, vali_data, vali_loader, criterion):
         self.model.eval()
         total_loss = []
         with torch.no_grad():
-            for i, (batch_x,batch_y,batch_x_mark,batch_y_mark,batch_y_in) in enumerate(vali_loader):
-                pred, true, sample = self._process_one_batch(
-                    vali_data, batch_x, batch_y, batch_x_mark, batch_y_mark,batch_y_in)
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark, batch_y_in) in enumerate(vali_loader):
+                true, sample = self._process_one_batch(
+                    vali_data, batch_x, batch_y, batch_x_mark, batch_y_mark, batch_y_in)
                 loss = criterion(sample, true)
                 total_loss.append(loss.item())
             total_loss = np.average(total_loss)
         return total_loss
 
     def train(self, setting):
-        train_data, train_loader = self._get_data(flag = 'train')
-        vali_data, vali_loader = self._get_data(flag = 'val')
-        test_data, test_loader = self._get_data(flag = 'test')
+        train_data, train_loader = self._get_data(flag='train')
+        vali_data, vali_loader = self._get_data(flag='val')
+        test_data, test_loader = self._get_data(flag='test')
 
         path = os.path.join(self.args.checkpoints, setting)
         if not os.path.exists(path):
             os.makedirs(path)
-        time_now = time.time()        
+        time_now = time.time()
         train_steps = len(train_loader)
-        early_stopping = EarlyStopping(patience=self.args.patience, verbose=True)
-        
+        early_stopping = EarlyStopping(
+            patience=self.args.patience, verbose=True)
+
         model_optim = self._select_optimizer()
-        criterion =  self._select_criterion()
+        criterion = self._select_criterion()
         if self.args.use_amp:
             scaler = torch.cuda.amp.GradScaler()
         for epoch in range(self.args.train_epochs):
@@ -164,22 +177,25 @@ class Exp_Model(Exp_Basic):
             train_loss = []
             self.model.train()
             epoch_time = time.time()
-            for i, (batch_x,batch_y,batch_x_mark,batch_y_mark,batch_y_in) in enumerate(train_loader):
-                #print(batch_x.shape)
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark, batch_y_in) in enumerate(train_loader):
+                # print(batch_x.shape)
                 iter_count += 1
                 model_optim.zero_grad()
                 true, sample = self._process_one_batch(
                     train_data, batch_x, batch_y, batch_x_mark, batch_y_mark, batch_y_in)
                 loss = criterion(sample, true)
                 train_loss.append(loss.item())
-                #print(loss.item())
-                if (i+1) % 100==0:
-                    print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
+                # print(loss.item())
+                if (i+1) % 100 == 0:
+                    print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(
+                        i + 1, epoch + 1, loss.item()))
                     speed = (time.time()-time_now)/iter_count
-                    left_time = speed*((self.args.train_epochs - epoch)*train_steps - i)
-                    print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
+                    left_time = speed * \
+                        ((self.args.train_epochs - epoch)*train_steps - i)
+                    print(
+                        '\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
                     iter_count = 0
-                    time_now = time.time()                
+                    time_now = time.time()
                 if self.args.use_amp:
                     scaler.scale(loss).backward()
                     scaler.step(model_optim)
@@ -188,7 +204,8 @@ class Exp_Model(Exp_Basic):
                     loss.backward()
                     model_optim.step()
 
-            print("Epoch: {} cost time: {}".format(epoch+1, time.time()-epoch_time))
+            print("Epoch: {} cost time: {}".format(
+                epoch+1, time.time()-epoch_time))
             train_loss = np.average(train_loss)
             vali_loss = self.vali(vali_data, vali_loader, criterion)
             test_loss = self.vali(test_data, test_loader, criterion)
@@ -204,7 +221,7 @@ class Exp_Model(Exp_Basic):
 
         best_model_path = path+'/'+'checkpoint.pth'
         self.model.load_state_dict(torch.load(best_model_path))
-        
+
         return self.model
 
     def test(self, setting):
@@ -212,8 +229,9 @@ class Exp_Model(Exp_Basic):
         self.model.eval()
         preds = []
         trues = []
-        for i, (batch_x,batch_y,batch_x_mark,batch_y_mark,batch_y_in) in enumerate(test_loader):
-            true, sample = self._process_one_batch(test_data, batch_x, batch_y, batch_x_mark, batch_y_mark,batch_y_in)
+        for i, (batch_x, batch_y, batch_x_mark, batch_y_mark, batch_y_in) in enumerate(test_loader):
+            true, sample = self._process_one_batch(
+                test_data, batch_x, batch_y, batch_x_mark, batch_y_mark, batch_y_in)
             preds.append(sample.detach().cpu().numpy())
             trues.append(true.detach().cpu().numpy())
         preds = np.array(preds)
@@ -229,7 +247,7 @@ class Exp_Model(Exp_Basic):
         trues = test_data.inverse_transform(trues)
 
         # result save
-        folder_path = './results/' + setting +'/'
+        folder_path = './results/' + setting + '/'
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
         return mae, mse
@@ -242,16 +260,17 @@ class Exp_Model(Exp_Basic):
             self.model.load_state_dict(torch.load(best_model_path))
         self.model.eval()
         preds = []
-        for i, (batch_x,batch_y,batch_x_mark,batch_y_mark,batch_y_in) in enumerate(pred_loader):
-            true, sample= self._process_one_batch(pred_data, batch_x, batch_y, batch_x_mark, batch_y_mark, batch_y_in)
+        for i, (batch_x, batch_y, batch_x_mark, batch_y_mark, batch_y_in) in enumerate(pred_loader):
+            true, sample = self._process_one_batch(
+                pred_data, batch_x, batch_y, batch_x_mark, batch_y_mark, batch_y_in)
             preds.append(pred.detach().cpu().numpy())
 
         preds = np.array(preds)
         preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
         preds = pred_data.inverse_transform(preds)
-        preds = preds[...,-1].reshape(-1, preds.shape[-2])
+        preds = preds[..., -1].reshape(-1, preds.shape[-2])
         # result save
-        folder_path = './results/' + setting +'/'
+        folder_path = './results/' + setting + '/'
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
         np.savetxt(folder_path+'real_prediction.txt', preds, fmt='%.4f')
@@ -260,23 +279,25 @@ class Exp_Model(Exp_Basic):
     def _process_one_batch(self, dataset_object, batch_x, batch_y, batch_x_mark, batch_y_mark, batch_y_in):
         batch_x = batch_x.float().to(self.device)
         batch_y = batch_y.float()
-        
+
         batch_x_mark = batch_x_mark.float().to(self.device)
         batch_y_mark = batch_y_mark.float().to(self.device)
 
         # decoder input
         dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
-        dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+        dec_inp = torch.cat(
+            [batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
         # encoder - decoder
         if self.args.use_amp:
             with torch.cuda.amp.autocast():
-                sample = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+                sample = self.model(batch_x, batch_x_mark,
+                                    dec_inp, batch_y_mark)
         else:
-            sample= self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+            sample = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
         if self.args.inverse:
             sample = dataset_object.inverse_transform(sample)
-        
-        f_dim = -1 if self.args.features=='MS' else 0
-        batch_y = batch_y[:,-self.args.pred_len:,f_dim:].to(self.device)
-        sample = sample[...,:,f_dim:]
+
+        f_dim = -1 if self.args.features == 'MS' else 0
+        batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
+        sample = sample[..., :, f_dim:]
         return batch_y, sample
